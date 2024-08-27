84609e0eade00c9a64d36d5c2834a9598579cfc9
Tanwie12 <adeytanwie@gmail.com>
2024-08-27 23:13:45 +0400

    did it

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -30,7 +30,7 @@ const Orders = async () => {
             <div className="flex flex-col gap-5">
               {order.products.map((orderItem: OrderItemType) => (
                 <div className="flex gap-4">
-                  <Image
+                  <img
                     src={orderItem.product.media[0]}
                     alt={orderItem.product.title}
                     width={100}
